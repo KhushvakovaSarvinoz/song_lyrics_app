39f8bd8dd701ea39e4aea74ca55a7620220d9dd1
Sarvinoz Xushvaqova <132272733+KhushvakovaSarvinoz@users.noreply.github.com>
2023-10-01 17:31:07 -0600

    The app had been added new script.js file
    +

---
+++ script.js
@@ -0,0 +1,6 @@
+// VARIABLES
+const form =document.getElementById("form");
+const search =document.getElementById("search");
+const result =document.getElementById("result");
+
+const apiURL = "http://api.lyrics.ovh"
