10f5471a245b0d59e70f5241cad73f487aa5bc71
Sarvinoz Xushvaqova <132272733+KhushvakovaSarvinoz@users.noreply.github.com>
2023-10-13 20:16:43 -0600

    This app have been added get input value
    +

--- script.js
+++ script.js
@@ -3,4 +3,16 @@ const form =document.getElementById("form");
 const search =document.getElementById("search");
 const result =document.getElementById("result");
 
-const apiURL = "http://api.lyrics.ovh"
+const apiURL = "http://api.lyrics.ovh";
+
+//Get input value
+form.addEventListener("submit", e =>{
+    e.preventDefault();
+    let searchValue = search.value.trim();
+
+    if(!searchValue){
+        alert("Nothing to search")
+    } else{
+        beginSearch(searchValue);
+    }
+})
